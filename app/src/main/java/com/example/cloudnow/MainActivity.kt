e9ca80d2204e220cc336d0cbbdf6fda584b6ce37
JB <jeevajb163@gmail.com>
2025-06-24 12:25:09 +0530

    Custom colors created.

--- app/src/main/java/com/example/cloudnow/MainActivity.kt
+++ app/src/main/java/com/example/cloudnow/MainActivity.kt
@@ -19,7 +19,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CloudNowTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
--- app/src/main/java/com/example/cloudnow/ui/theme/Color.kt
+++ app/src/main/java/com/example/cloudnow/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.example.cloudnow.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val DarkBackground = Color(0xFF1E1E1E)
+val DarkPrimaryText = Color(0xFFE0E0E0)
+val DarkSecondaryText = Color(0xFFB0B0B0)
+val DarkAccent = Color(0xFFFF0099)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightBackground = Color(0xFFFFFFFF)
+val LightPrimaryText = Color(0xFF212121)
+val LightSecondaryText = Color(0xFF616161)
+val LightAccent = Color(0xFF4285F4)
